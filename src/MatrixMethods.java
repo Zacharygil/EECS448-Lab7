aa763edcd74654ffceb09d3cc0f7de9edd5301cf
Zacharygil <Zacula@Zacharys-MacBook-Pro-7.local>
2017-03-30 10:11:30 -0500

    Constructor and Methods

---
+++ src/MatrixMethods.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Zacula on 3/30/17.
+ */
+public class MatrixMethods {
+    //Globals
+    double [][] matrix;
+    int GenSize = 0;
+
+
+    //Make the matrix
+    public MatrixMethods(int InputSize){
+        GenSize = InputSize;
+        matrix = new double[GenSize][GenSize];
+
+    }
+
+
+
+}
